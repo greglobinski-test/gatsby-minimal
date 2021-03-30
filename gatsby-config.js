7c3f5759c9b0a9281f77ee29b02cd9a9db46a09e
Greg Lobinski <greglobinski@gmail.com>
2021-03-30 11:01:42 +0200

    fix: update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: "gatsby-minimal",
+    title: 'gatsby-minimal',
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    {
+      resolve: 'gatsby-plugin-functions',
+      options: {
+        // You can customize this path where your store functions but they
+        // will always be served from {site_url}/functions/{file_name}
+        path: `${__dirname}/src/functions`,
+      },
+    },
+  ],
 };
