5b21383290129bcce1f39388c508106a73b9bd4a
Greg Lobinski <greglobinski@gmail.com>
2021-03-31 14:01:08 +0200

    add api

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,29 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
-    title: 'gatsby-minimal',
+    title: 'gatsby-hosting-canary',
   },
-  plugins: ['gatsby-plugin-gatsby-cloud'],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-functions`,
+      options: {
+        path: path.join(__dirname, `src/api`),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-gatsby-cloud`,
+      options: {
+        headers: {
+          '/*': [`X-Test-Header: this is the header for /*`],
+          '/client-only/*': [
+            `X-Test-Header: this is the header for /client-only/*`,
+          ],
+          '/static-page': [
+            `X-Test-Header: this is the header for /static-page`,
+          ],
+        },
+      },
+    },
+  ],
 };
--- src/functions/test.js
+++
@@ -1,9 +0,0 @@
-// src/functions/test.js
-
-export default (req, res) => {
-  console.log(req.body);
-  res.status(200).send({
-    message: 'Hello, you.',
-    date: Date.now(),
-  });
-};
